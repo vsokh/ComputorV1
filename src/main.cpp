2a654522774c8a68ba28bc861d9557514cb678bd
vsokh <vsokolohorskyi@gmail.com>
2022-01-29 19:08:27 +0200

    Computor is able to solve an equation

--- src/main.cpp
+++ src/main.cpp
@@ -1,81 +1,162 @@
 #include <iostream>
 #include <tuple>
+#include <vector>
 
 #include "Lexer.h"
 #include "Token.h"
 
-struct Term {
+struct Term
+{
     double coef;
-    std::uint8_t deg;
+    int deg;
 };
 
-class TermParser {
-public:
-    Term parse(const std::string& term);
+std::vector<Term> toTerms(std::vector<Token> tokens)
+{
+    std::vector<Term> terms;
 
-private:
-    Lexer _lexer;
-};
+    /* bool unknownIsNext = false; */
+    Term term;
+    double sign = 1;
+    double wasEq = 1;
 
-struct Expression {
-    // x0, x1, x2
-    std::tuple<double, double, double> terms;
-};
+    for (auto it = tokens.begin(); it != tokens.end(); ++it)
+    {
+        switch (it->kind)
+        {
+            case TokenKind::End:
+            {
+                return terms;
+            }
+            case TokenKind::Equality:
+            {
+                wasEq = -1;
+                break;
+            }
+            case TokenKind::Num:
+            {
+                term.coef += *it->value * sign * wasEq;
+                break;
+            }
+            case TokenKind::Mul:
+            {
+                /* unknownIsNext = true; */
+                break;
+            }
+            case TokenKind::Sub:
+            {
+                sign *= sign > 0 ? -1 : 1;
+                break;
+            }
+            case TokenKind::Add:
+            {
+                sign *= sign < 0 ? -1 : 1;
+                break;
+            }
+            case TokenKind::Unknown:
+            {
+                term.deg = *it->value;
+                terms.push_back(term);
 
-class ExpressionParser {
-public:
-    ExpressionParser(std::string&& str);
-    Expression get();
-
-private:
-    TermParser _termParser;
-};
-
-class ExpressionWhiteSpaceCleaner {
-public:
-    explicit ExpressionWhiteSpaceCleaner(std::string&& str);
-    std::string get();
-
-private:
-    std::string _str;
-};
-
-class ExpressionSplitter {
-public:
-    explicit ExpressionSplitter(std::string&& str);
-    std::string get();
-
-private:
-    std::string _str;
-};
+                sign = 1;
+                /* unknownIsNext = false; */
+                term = Term{};
+                break;
+            }
+        }
+    }
+    return terms;
+}
 
 int main(int argc, char **argv)
 {
-    auto expr = creator.create();
-
-    (void)argc; (void)argv;
-
-    /* auto lexer = Lexer{argv[1]}; */
-    /* t0 = 9 - 49 = -40 */
-    /* t1 = 16 - 86 = -70 */
-    /* t2 = -21 */
-
-    // reducedForm: - 40 * X^0 - 70 * X^1 - 21 X^2 = 0
-    // firstTerm := sub num mul unknown | num mul unknown
-    // otherTerms := sub or add num unknown
-    // Parser - The purpose is to sum up all unknowns into 3 possible buckets: x0, x1, x2.
-        // 1 iteration creates a sequence of terms
-        // 2 iteration puts all terms into the buckets
-    try
+    // TODO: check on sematic mistakes:
+    // 1. No equality sign
+    // 2. A term which doesn't follow the form - a*X^p
+    // 3. etc
+    // TODO: Support higher term's degree
+    (void)argv;
+    if (argc <= 1)
     {
-        auto parser = Parser{" 9 * X^0 - 21 * X^2 + 16 * X^1 = 86 * X^1 + 49 * X^0"};
-        auto expression = parser.parse();
-        (void)expression;
+        return 1;
     }
-    catch (const std::exception& e)
+
+    std::string expr = argv[1];
+    /* 1 roots */
+    /* std::string expr = "5 * X^0 + 4 * X^1 = 4 * X^0"; */
+    /* 2 roots */
+    /* std::string expr = "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0"; */
+
+
+    // ------------- string to tokens --------------
+    auto lexer = Lexer{expr};
+    auto token = lexer.getNextToken();
+    std::vector<Token> tokens;
+    while (token != TokenKind::End)
     {
-        std::cout << e.what() << std::endl;
+        tokens.push_back(token);
+        token = lexer.getNextToken();
     }
-    std::cout << std::endl;
+    // ---------------------------------------------
+
+
+    // ----------- tokens to expression ------------
+    auto terms = toTerms(tokens);
+    std::sort(terms.begin(),
+              terms.end(),
+              [](const Term& lhs, const Term& rhs)
+              {
+                  return lhs.deg < rhs.deg;
+              });
+
+    Term x0{0, 0}, x1{0, 1}, x2{0, 2};
+    for (auto term : terms)
+    {
+        if (term.deg == 0) x0.coef += term.coef;
+        if (term.deg == 1) x1.coef += term.coef;
+        if (term.deg == 2) x2.coef += term.coef;
+    }
+    // --------------------------------------------
+
+
+    // ------------ show reduced form -------------
+    std::cout << "Reduced form: ";
+    if (x0.coef < 0.0)      std::cout << "- " << (-1*x0.coef) << " * X^0";
+    else if (x0.coef > 0.0) std::cout << x0.coef << " * X^0";
+
+    if (x1.coef < 0.0)      std::cout << " - " << (-1*x1.coef) << " * X^1";
+    else if (x1.coef > 0.0) std::cout << " + " << x1.coef << " * X^1";
+
+    if (x2.coef < 0.0)      std::cout << " - " << (-1*x2.coef) << " * X^2";
+    else if (x2.coef > 0.0) std::cout << " + " << x2.coef << " * X^2";
+    std::cout << " = 0" << std::endl;
+    // --------------------------------------------
+
+
+    // --------- exrepssion to a solution ---------
+    auto degree = 0;
+    degree = x1.coef != 0.0 ? x1.deg : degree;
+    degree = x2.coef != 0.0 ? x2.deg : degree;
+    std::cout << "Polynomial degree: " << degree << std::endl;
+    std::cout << "The solution is:\n";
+    if (degree == 0)
+    {
+    }
+    else if (degree == 1)
+    {
+        std::cout << -1*x0.coef / x1.coef << std::endl;
+    }
+    else if (degree == 2)
+    {
+        double a = x2.coef;
+        double b = x1.coef;
+        double c = x0.coef;
+        double D = b*b - 4*a*c;
+        double r1 = (-1*b - std::sqrt(D))/(2*a);
+        double r2 = (-1*b + std::sqrt(D))/(2*a);
+        std::cout << r1 << std::endl;
+        std::cout << r2 << std::endl;
+    }
+    // ---------------------------------------------
     return 0;
 }
